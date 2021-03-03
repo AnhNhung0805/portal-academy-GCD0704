80b0d0139c93db07311bc75da0cdb3bd1efbc978
AnhNhung0805 <65988480+AnhNhung0805@users.noreply.github.com>
2021-03-03 20:18:03 +0700

    Change, confirm password profile

--- ViewModels/ProfileViewModel.cs
+++ ViewModels/ProfileViewModel.cs
@@ -18,4 +18,13 @@ namespace AcademicPortal.ViewModels
 		public StaffInfo StaffInfo { get; set; }
 		public TrainerInfo TrainerInfo { get; set; }
 	}
+	[StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 1)]
+		[DataType(DataType.Password)]
+		[Display(Name = "Password")]
+		public string Password { get; set; }
+
+		[DataType(DataType.Password)]
+		[Display(Name = "Confirm password")]
+		[Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+		public string ConfirmPassword { get; set; }
 }
